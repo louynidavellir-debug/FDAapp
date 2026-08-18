41050d4ef6fff0137f42207933f75988128ebbdc
FDA <louynidavellir@gmail.com>
2026-08-17 21:55:38 -0300

    app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'filhos-asgard-v20260817-loginfix-all-users';
+const CACHE_NAME = 'filhos-asgard-v20260817-loginfix-all-users-icon90';
 const ASSETS = [
     './', './index.html', './style.css', './app.js', './cloud.js', './firebase-config.js',
     './manifest.json', './icons/icon-192-v17.png', './icons/icon-512-v17.png', './icons/logo-asgard.png'
