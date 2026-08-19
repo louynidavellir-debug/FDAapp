b191af590a59a0d1de0737a989bd61ffa385d5f9
FDA <louynidavellir@gmail.com>
2026-08-19 17:40:04 -0300

    Versão 1.0.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'asgard-v-final-audit-alerts-v1';
+const CACHE_NAME = 'asgard-v-final-audit-alerts-v2';
 const ASSETS = [
     './', './index.html', './style.css', './app.js', './cloud.js', './firebase-config.js',
     './manifest.json', './icons/icon-192-v17.png', './icons/icon-512-v17.png', './icons/logo-asgard.png'
